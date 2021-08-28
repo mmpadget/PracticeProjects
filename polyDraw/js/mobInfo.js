66d11559ebe2b6843bde786486f1b99e3b641514
dogmanstar <danielmpadget@gmail.com>
2021-08-27 20:18:31 -0700

    new

--- polyDraw/js/mobInfo.js
+++ polyDraw/js/mobInfo.js
@@ -1,41 +1,42 @@
 export { levelData };
 
 
-//[mobType, cenX, cenY, genus, movesIn, shoots, zStack]
+//[mobType, cenX, cenY, genus, movesIn, shoots, zStack, speed]
 class levelData {
   constructor() {
     this.level_1 = {
       wave1: [
-        ['starMob', 200, 200, 5, 30, false, 2],
-        ['starMob', 300, 200, 5, 30, false, 2],
-        ['starMob', 400, 200, 5, 30, false, 2],
-        ['starMob', 500, 200, 5, 30, false, 2],
-        ['squareMob', 450, 300, 10, 30, true, 1],
-        ['squareMob', 150, 300, 10, 30, true, 1]
+        ['starMob', 200, 200, 5, 30, false, 2, .5],
+        ['polyMob', 150, 100, 5, 50, false, 2, 1.25],
+        ['starMob', 300, 200, 5, 30, false, 2, .6],
+        ['starMob', 400, 200, 5, 30, false, 2, .7],
+        ['starMob', 500, 200, 5, 30, false, 2, .8],
+        ['squareMob', 450, 300, 10, 30, true, 1, 1],
+        ['squareMob', 150, 300, 10, 30, true, 1, 1]
       ],
       wave2: [
-        ['polyMob', 150, 100, 5, 50, false, 2],
-        ['polyMob', 200, 100, 5, 50, false, 2],
-        ['polyMob', 250, 100, 5, 50, false, 2],
-        ['polyMob', 300, 100, 5, 50, false, 2],
-        ['polyMob', 350, 100, 5, 50, false, 2],
-        ['polyMob', 400, 100, 5, 50, false, 2],
-        ['polyMob', 450, 100, 5, 50, false, 2],
-        ['squareMob', 300, 300, 10, 30, true, 1]
+        ['polyMob', 150, 100, 5, 50, false, 2, 1.75],
+        ['polyMob', 200, 100, 5, 50, false, 2, 1.75],
+        ['polyMob', 250, 100, 5, 50, false, 2, 1.75],
+        ['polyMob', 300, 100, 5, 50, false, 2, 1.75],
+        ['polyMob', 350, 100, 5, 50, false, 2, 1.75],
+        ['polyMob', 400, 100, 5, 50, false, 2, 1.75],
+        ['polyMob', 450, 100, 5, 50, false, 2, 1.75],
+        ['squareMob', 300, 300, 10, 30, true, 1, 1]
       ],
       wave3: [
-        ['polyMob', 150, 100, 5, 50, false, 2],
-        ['starMob', 200, 200, 5, 30, false, 2],
-        ['squareMob', 300, 300, 10, 30, true, 1]
+        ['polyMob', 150, 100, 5, 50, false, 2, 1.75],
+        ['starMob', 200, 200, 5, 30, false, 2, 4],
+        ['squareMob', 300, 300, 10, 30, true, 1, 1]
       ],
       wave4: [
-        ['polyMob', 150, 100, 5, 50, false, 2],
-        ['starMob', 200, 200, 5, 30, false, 2],
-        ['squareMob', 300, 300, 10, 30, true, 1]
+        ['polyMob', 150, 100, 5, 50, false, 2, 1.75],
+        ['starMob', 200, 200, 5, 30, false, 2, 2],
+        ['squareMob', 300, 300, 10, 30, true, 1, 1]
       ],wave5: [  //Boss wave
-        ['polyMob', 150, 100, 5, 50, false, 2],
-        ['starMob', 200, 200, 5, 30, false, 2],
-        ['squareMob', 300, 300, 10, 30, true, 1]
+        ['polyMob', 150, 100, 5, 50, false, 2, 1.75],
+        ['starMob', 200, 200, 5, 30, false, 2, .8],
+        ['squareMob', 300, 300, 10, 30, true, 1, 1]
       ]
     }
     this.level_2 = [
